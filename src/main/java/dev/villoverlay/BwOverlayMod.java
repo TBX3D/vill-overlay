41dfd7b624d22a0ab8a44a2227d88db5c3498fdf
TBX3D <88289044+TBX3D@users.noreply.github.com>
2026-06-22 15:49:36 -0700

    release: bump version to 1.1.0

--- src/main/java/dev/villoverlay/BwOverlayMod.java
+++ src/main/java/dev/villoverlay/BwOverlayMod.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ThreadFactory;
 public class BwOverlayMod {
 
     public static final String MODID = "villoverlay";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     private static KeyBinding keyToggle;
     private static KeyBinding keyRefresh;
